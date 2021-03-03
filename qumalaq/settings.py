e49e51bcdff5c514e7694737a01cf79636768c3f
Sundetaly <sundetaliomirzak@gmail.com>
2021-03-04 00:43:44 +0600

    update

--- qumalaq/settings.py
+++ qumalaq/settings.py
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-Ru'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Almaty'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
